18a9557b4500e8caf3d7f36a3df13c536206f3f2
teachergg <jwhitney@bisd.us>
2022-03-28 23:56:57 -0500

    added userInput validation

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)'
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
       message: 'Enter your GitHub Username'
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your Github name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -45,11 +61,27 @@ Add a New Project
       type: 'input',
       name: 'name',
       message: 'What is the name of your project?'
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log('Please enter a project name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
       message: 'Provide a description of the project (Required)'
+      validate: descInput => {
+        if (descInput) {
+          return true;
+        } else {
+          console.log('Please enter your project description.');
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -61,6 +93,14 @@ Add a New Project
       type: 'input',
       name: 'link',
       message: 'Enter the GitHub link to your project. (Required)'
+      validate: linkInput => {
+        if (linkInput) {
+          return true;
+        } else {
+          console.log('Please enter a Github link to your project.');
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
